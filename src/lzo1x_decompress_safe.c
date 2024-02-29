b9afd4c567498d09708e061be253b355d2b46647
HaMeNopeDiot <staruhind@yandex.ru>
2024-02-29 18:01:06 +0300

    add comentaries

--- src/lzo1x_decompress_safe.c
+++ src/lzo1x_decompress_safe.c
@@ -4,8 +4,8 @@ int lzo1x_decompress_safe(const unsigned char *in, size_t in_len,
 			  unsigned char *out, size_t *out_len)
 {
 	
-	unsigned char *op;
-	const unsigned char *ip;
+	unsigned char *op; // Pointer from out
+	const unsigned char *ip; // Pointer from in
 	size_t t, next;
 	size_t state = 0;
 	const unsigned char *m_pos;
@@ -14,15 +14,15 @@ int lzo1x_decompress_safe(const unsigned char *in, size_t in_len,
 
 	unsigned char bitstream_version;
 
-	op = out;
-	ip = in;
+	op = out; // Set op to out buffer
+	ip = in;  // Set ip to in buffer
 
-	if (unlikely(in_len < 3))
+	if (unlikely(in_len < 3)) // size of comp info must be more than 3 bytes
 		goto input_overrun;
 
-	if (likely(in_len >= 5) && likely(*ip == 17)) {
+	if (likely(in_len >= 5) && likely(*ip == 17)) { // set bitstream if start of pointer is 17.
 		bitstream_version = ip[1];
-		ip += 2;
+		ip += 2; // jump 2 next
 	} else {
 		bitstream_version = 0;
 	}
@@ -53,7 +53,7 @@ int lzo1x_decompress_safe(const unsigned char *in, size_t in_len,
 						return LZO_E_ERROR;
 
 					offset = (offset << 8) - offset;
-					t += offset + 15 + *ip++;
+					t += offset + 15 + *ip++; // t is a nearest compressied information
 				}
 				t += 3;
 copy_literal_run:
@@ -62,20 +62,20 @@ copy_literal_run:
 					NEED_IP(t + 3);
 					do {
 						*op++ = *ip++;
-					} while (--t > 0);
+					} while (--t > 0); // program start with searching a place where program can find a comprassied information. Also they copy in buffer to out untill program find comprassied info.
 				}
 				state = 4;
 				continue;
 			} else if (state != 4) {
-				next = t & 3;
+				next = t & 3; 
 				m_pos = op - 1;
 				m_pos -= t >> 2;
 				m_pos -= *ip++ << 2;
 				TEST_LB(m_pos);
 				NEED_OP(2);
-				op[0] = m_pos[0];
-				op[1] = m_pos[1];
-				op += 2;
+				op[0] = m_pos[0]; // copy first byte
+				op[1] = m_pos[1]; // copy second byte
+				op += 2; // pointer -> 2
 				goto match_next;
 			} else {
 				next = t & 3;
@@ -85,13 +85,15 @@ copy_literal_run:
 				t = 3;
 			}
 		} else if (t >= 64) {
-			next = t & 3;
-			m_pos = op - 1;
-			m_pos -= (t >> 2) & 7;
-			m_pos -= *ip++ << 3;
-			t = (t >> 5) - 1 + (3 - 1);
+			next = t & 3; // this is S in 0 1 L D D D S S
+			// m_pos is a distance = (H << 3) + D + 1
+			m_pos = op - 1; // This is plus one in formula
+			m_pos -= (t >> 2) & 7; // This is D in formule 
+			m_pos -= *ip++ << 3; // This is H << 3 in formula 
+			t = (t >> 5) - 1 + (3 - 1); // Len, which Len = 3 + L
 		} else if (t >= 32) {
-			t = (t & 31) + (3 - 1);
+			t = (t & 31) + (3 - 1); // t & 31 is just a mask of 0 0 1 L L L L L, where added +2
+			// this is just a formula 2 + L, from lzo.txt.
 			if (unlikely(t == 2)) {
 				size_t offset;
 				const unsigned char *ip_last = ip;
@@ -109,9 +111,9 @@ copy_literal_run:
 				NEED_IP(2);
 			}
 			m_pos = op - 1;
-			next = get_unaligned_le16(ip);
-			ip += 2;
-			m_pos -= next >> 2;
+			next = get_unaligned_le16(ip); // read 2 bytes in little endian
+			ip += 2; // ip pointer go to end of coded information(to last byte)
+			m_pos -= next >> 2; // go to reference (<-) 
 			next &= 3;
 		} else {
 			NEED_IP(2);
@@ -160,15 +162,15 @@ copy_literal_run:
 		}
 		TEST_LB(m_pos);
 		{
-			unsigned char *oe = op + t;
+			unsigned char *oe = op + t; // here programm copy from reference information: do from compared text to source
 			NEED_OP(t);
-			op[0] = m_pos[0];
-			op[1] = m_pos[1];
-			op += 2;
-			m_pos += 2;
+			op[0] = m_pos[0]; // save first byte
+			op[1] = m_pos[1]; // save second byte
+			op += 2; // always it's more than 2 bytes.
+			m_pos += 2; // pointer from source to -> +2
 			do {
-				*op++ = *m_pos++;
-			} while (op < oe);
+				*op++ = *m_pos++; // copy all bytes
+			} while (op < oe); // untill op reach oe
 		}
 match_next:
 		state = next;
